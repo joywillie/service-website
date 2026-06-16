7ae2e6b6fb6e2e95be3cf23634dd8f5b3eb80355
Joy William <jw42205769@gmail.com>
2026-06-16 16:26:07 +0300

    Update user.js

--- routes/model/user.js
+++ routes/model/user.js
@@ -1,9 +1,31 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String
-});
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
 
-module.exports = mongoose.model("User", UserSchema);
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+
+// Prevent model overwrite errors
+module.exports = mongoose.models.User || mongoose.model("User", UserSchema);
